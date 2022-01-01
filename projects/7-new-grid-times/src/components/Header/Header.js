f103c9f5812e9ad4e76a7cfc06b5ba3495a733b1
Charlie Hebert <charlie.hebert92@gmail.com>
2022-01-01 23:57:10 +0000

    7-1: Header

--- projects/7-new-grid-times/src/components/Header/Header.js
+++ projects/7-new-grid-times/src/components/Header/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { QUERIES } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
   return (
@@ -28,17 +28,45 @@ const Header = () => {
           </ActionGroup>
         </Row>
       </SuperHeader>
-      <MainHeader>
-        <Logo />
-      </MainHeader>
+      <ResponsiveHeader />
     </header>
   );
 };
+
+function ResponsiveHeader() {
+  return (
+    <>
+      <MobileHeader>
+        <Logo />
+      </MobileHeader>
+
+      <DesktopHeader>
+        <ActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </ActionGroup>
+        <Logo />
+        <SubscribeWrapper>
+          <Button>Subscribe</Button>
+          <Link href="">Already a subscriber?</Link>
+        </SubscribeWrapper>
+      </DesktopHeader>
+    </>
+  );
+}
 
 const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -61,10 +89,43 @@ const ActionGroup = styled.div`
 
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
-  align-items: center;
+  align-items: baseline;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    margin-top: 48px;
+    margin-bottom: 64px;
+  }
+`;
+
+const MobileHeader = styled(MainHeader)`
+  @media ${QUERIES.desktopAndUp} {
+    display: none;
+  }
+`;
+
+const DesktopHeader = styled(MainHeader)`
+  display: none;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+const SubscribeWrapper = styled.div`
+  position: relative;
+  top: -8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Link = styled.a`
+  padding: 8px;
+  text-decoration: underline;
 `;
 
 export default Header;
